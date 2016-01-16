69df3842906980344c8e05f6ff313f4e09945109
Aaron Crawfis <acrawfis@nd.edu>
2016-01-16 16:04:32 -0500

    Initial Commit

---
+++ TownSquares/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TownSquares
+//
+//  Created by Aaron Crawfis on 1/16/16.
+//  Copyright © 2016 Aaron Crawfis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TownSquares/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TownSquares
+//
+//  Created by Aaron Crawfis on 1/16/16.
+//  Copyright © 2016 Aaron Crawfis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
