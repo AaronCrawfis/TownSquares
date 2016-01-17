a44c67565bbb585bae1c66378fc5f93b3f2f1316
Aaron Crawfis <acrawfis@nd.edu>
2016-01-16 22:44:50 -0500

    Added multiple modal view pop

--- TownSquares/driverEmployableVC.m
+++ TownSquares/driverEmployableVC.m
@@ -24,15 +24,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)yesButtonPressed:(UIButton *)sender
 {
@@ -40,7 +31,12 @@
     
     // Send Data to Server
     
+    
+    // Perform Segue
+    [self performSegueWithIdentifier:@"TYSegue" sender:self];
+    
 }
+
 
 - (IBAction)noButtonPressed:(UIButton *)sender
 {
